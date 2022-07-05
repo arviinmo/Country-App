398f3ae18a230ebae186a121e5c84bad2927adca
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-07-05 18:07:26 +0430

    added world-countries-capital package to project

--- ui.js
+++ ui.js
@@ -1,11 +1,26 @@
-'use strict';
-const React = require('react');
-const {Text} = require('ink');
+"use strict";
+const React = require("react");
+const { Box } = require("ink");
+const TextInput = require("ink-text-input").default;
+const wcc = require("world-countries-capitals");
 
-const App = ({name = 'Stranger'}) => (
-	<Text>
-		Hello, <Text color="green">{name}</Text>
-	</Text>
-);
+const App = () => {
+	const [country, setCountry] = React.useState("");
+	const [capital, setCapital] = React.useState("");
+	const [currency, setCurrency] = React.useState("");
+	const [phone, setPhone] = React.useState("");
+
+	
+
+	return (
+		<Box>
+			<TextInput
+				placeholder="Enter country..."
+				value={country}
+				onChange={setCountry}
+			/>
+		</Box>
+	);
+};
 
 module.exports = App;
